fabdebe52178ebe6dcdafcfdc7cc355c021b94a1
mikevgb <75221782+mikevgb@users.noreply.github.com>
2020-12-17 18:50:46 +0100

    Update move_hombrew.sh

--- move_hombrew.sh
+++ move_hombrew.sh
@@ -2,9 +2,7 @@
 
 rm -rf /goinfre/${USER}/Homebrew
 rm -rf /goinfre/${USER}/.brew
-mkdir -p /goinfre/${USER}/Hombrew
-mkdir -p /goinfre/${USER}/.brew
-rm -rf $HOME/Library/Caches/Homebrew
-rm -rf $HOME/.brew
+mv $HOME/Library/Caches/Homebrew /goinfre/${USER}/Hombrew
+mv $HOME/.brew /goinfre/${USER}/.brew
 ln -s /goinfre/${USER}/Homebrew $HOME/Library/Caches/Homebrew
 ln -s /goinfre/${USER}/.brew $HOME/.brew
